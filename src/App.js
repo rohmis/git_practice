41fb65b30665b282321374de2d766f03f4765abd
rohmis <romishra@bestpeers.in>
2023-12-08 12:22:45 +0530

    deepak added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ function App() {
   return (
     <div className="App">
        <h1>Rohit</h1>
-<<<<<<< new-feature
+
        <h1>is a good person</h1>
-=======
-       <h1>mishra</h1>
->>>>>>> main
-    </div>
+       <h1>Deepak</h1>
+  </div>
   );
 }
 
